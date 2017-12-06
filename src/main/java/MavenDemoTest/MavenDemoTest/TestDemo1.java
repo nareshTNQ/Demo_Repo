1c2b2709a1e8b8e2a0f2ddf3adf4b2809e88ce7f
Naresh <naresh.vodlamani@tnqsoftware.co.in>
2017-12-06 14:37:02 +0530

    test

---
+++ src/main/java/MavenDemoTest/MavenDemoTest/TestDemo1.java
@@ -0,0 +1,29 @@
+package MavenDemoTest.MavenDemoTest;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * 
+ * Author : Naresh v
+ * 
+ * */
+
+public class TestDemo1 {
+	
+	
+    @Test
+	public void demo() {
+    	
+    	System.out.println("test3");
+		
+	}
+
+    @Test
+	public void demo1() {
+		
+		System.out.println("test4");
+		
+	}
+
+}
---
+++ src/main/java/MavenDemoTest/MavenDemoTest/TestDemo2.java
@@ -0,0 +1,22 @@
+package MavenDemoTest.MavenDemoTest;
+
+import org.testng.annotations.Test;
+
+public class TestDemo2 {
+	
+	
+    @Test
+	public void demo3() {
+		
+		System.out.println("test3");
+		
+	}
+
+    @Test
+	public void dem41() {
+		
+		System.out.println("test4");
+		
+	}
+
+}
